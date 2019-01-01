61662533ee43d12a8a0f1cea540fdd72ebe00ad1
Aries Lustracion <lustracion.aries25@gmail.com>
2019-01-02 02:26:29 +0800

    changes

--- app.py
+++ app.py
@@ -10,38 +10,189 @@ app = Flask(__name__)
 app.secret_key = config.SECRET_KEY
 Bootstrap(app)
 
+# ============ LOGIN =============
+
 @app.route('/')
 def login():
-  return render_template('login.html')
+    return render_template('login.html')
+
 
 @app.route('/login', methods=["GET", "POST"])
 def checkLogin():
-  result = db.authenticateLogin(request.form.get('userID'), request.form.get('userPassword'))
-  if len(result) == 1:
-    session['userID'] = result[0][0]
-    return str(result[0][2])
-  else:
-    return '0'
+    result = db.authenticateLogin(request.form.get('userID'), request.form.get('userPassword'))
+    if len(result) == 1:
+        session['userID'] = result[0][0]
+        return str(result[0][2])
+    else:
+        '0'
+
+# ============ NAVBAR =============
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
+
 
 @app.route('/home')
 def home():
-  return render_template('home.html', userDetails=db.getUserDetails(session['userID']))
+    try:
+        session.get('userID')
+        return render_template('home.html', userDetails=db.getUserDetails(session['userID']), classes=db.getStudentClasses(session['userID']))
+    except Exception as e:
+        return redirect('/')
 
-@app.route('/students')
-def students():
-  return render_template('students.html', students=db.getStudents())
 
-@app.route('/subjects')
-def subjects():
-  return render_template('subjects.html', subjects=db.getSubjects())
+# @app.route('/subjects')
+# def subjects():
+#     return render_template('subjects.html', subjects=db.getSubjects())
+
 
 @app.route('/pre-enrollment')
 def enrollment():
-  return render_template('pre-enrollment.html')
+    return render_template('pre-enrollment.html', classes=db.getClassesToEnroll(session['userID']))
+
+@app.route('/enrollSubject', methods=['POST'])
+def enrollSubject():
+    try:
+        db.enroll(request.form.get('stubCode'), session['userID'])
+        return 'True'
+    except:
+        return 'False'
 
 @app.route('/ratings')
 def ratings():
-  return render_template('ratings.html')
+    return render_template('ratings.html', teachers=db.getTeachers())
+
+
+# ======== ADMIN ==========
+@app.route('/admin')
+def admin():
+    return render_template('./Admin/Students/index.html')
+
+@app.route('/adminStudents')
+def adminStudents():
+    return render_template('./Admin/Students/index.html', students=db.getStudents())
+
+@app.route('/adminProfessors')
+def adminTeacher():
+    return render_template('./Admin/Teachers/teacher.html', teachers=db.getTeachers())
+
+@app.route('/adminClasses')
+def adminClasses():
+    return render_template('./Admin/Classes/classes.html', classSched=db.getClasses())
+
+@app.route('/adminSubjects')
+def adminSubjects():
+    return render_template('./Admin/Subject/subject.html', subjects=db.getSubjects())
+
+# ============ STUDENTS =============
+
+@app.route('/addStudent', methods=["GET", "POST"])
+def addStudent():
+    studentID = request.form.get('id')
+    fname = request.form.get('fname')
+    lname = request.form.get('lname')
+    course = request.form.get('course')
+    year = request.form.get('year')
+
+    if db.addStudent(studentID, fname, lname, course, year) is True:
+        return "success"
+    else:
+        return "fail"
+
+
+@app.route('/editStudent', methods=["GET", "POST"])
+def editStudent():
+    studentID = request.form.get('id')
+    fname = request.form.get('fname')
+    lname = request.form.get('lname')
+    course = request.form.get('course')
+    year = request.form.get('year')
+
+    if db.editStudent(studentID, fname, lname, course, year) is True:
+        return "success"
+    else:
+        return "fail"
+
+# ============ TEACHER =============
+
+@app.route('/addTeacher', methods=["GET", "POST"])
+def addTeacher():
+    teacherID = request.form.get('tid')
+    tfname = request.form.get('tfname')
+    tlname = request.form.get('tlname')
+
+    if db.addTeacher(teacherID, tfname, tlname) is True:
+        return "success"
+    else:
+        return "fail"
+
+@app.route('/editTeacher', methods=["GET", "POST"])
+def editTeacher():
+    teacherID = request.form.get('tid')
+    tfname = request.form.get('tfname')
+    tlname = request.form.get('tlname')
+
+    if db.editTeacher(teacherID, tfname, tlname) is True:
+        return "success"
+    else:
+        return "fail"
+
+# ============ CLASSES =============
+
+@app.route('/addClasses', methods=["GET", "POST"])
+def addClasses():
+    code = request.form.get('code')
+    cTid = request.form.get('cTid')
+    cSid = request.form.get('cSid')
+    sched = request.form.get('sched')
+    start = request.form.get('start')
+    end = request.form.get('end')
+
+    if db.addClasses(code, cTid, cSid, sched, start, end) is True:
+        return "success"
+    else:
+        return "fail"
+
+@app.route('/editClasses', methods=["GET", "POST"])
+def editClasses():
+    code = request.form.get('code')
+    cTid = request.form.get('cTid')
+    cSid = request.form.get('cSid')
+    sched = request.form.get('sched')
+    start = request.form.get('start')
+    end = request.form.get('end')
+
+    if db.editClasses(code, cTid, cSid, sched, start, end) is True:
+        return "success"
+    else:
+        return "fail"
+
+# ============ RATING =============
+
+@app.route('/saveRating', methods=["GET", "POST"])
+def saveRating():
+    try:
+        teacherID = request.form.get('teacherID')
+        studentID = session['userID']
+        rating = request.form.get('rating')
+        comment = request.form.get('comment')
+
+        db.rateTeacher(teacherID, studentID, rating, comment)
+
+        return 'success'
+    except:
+        return 'failed'
+
+
+# ======== CATCH EXCEPTIONS ==========
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    # returns a 200 (not a 404) with the following contents:
+    return "THIS PAGE DOES NOT EXIST!"
+
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
--- db.py
+++ db.py
@@ -1,7 +1,6 @@
 from config import config
 import mysql.connector as mysql
 from mysql.connector import errorcode
-import datetime
 
 # Try if system can connect to database
 try:
@@ -21,30 +20,101 @@ def getStudents():
     cur.execute("SELECT * FROM students")
     return cur.fetchall()
 
-def addStudents(fname, lname, course, year):
-    now = datetime.datetime.now()
-    randID = f"{now.year}-{now.month}{now.day}-{now.hour}{now.minute}"
+
+def addStudent(_id, fname, lname, course, year):
     try:
-        cur.execute("INSERT INTO students(studentID, studentFname, studentLname, studentCourse, studentYear) VALUES ('{randID}', '{fname}', '{lname}', '{course}', {year}")
+        cur.execute(f"INSERT INTO students VALUES ('{_id}', '{fname}', '{lname}', '{course}', {year})")
+        cur.execute(f"INSERT INTO loginusers VALUES ('{_id}','123',2)")
         return True
-    except:
+    except Exception as e:
         return False
+
+
+def editStudent(id, fname, lname, course, year):
+    try:
+        cur.execute(f"UPDATE students SET studentFname='{fname}', studentLname='{lname}', studentCourse='{course}', studentYear={year} WHERE studentID='{id}'")
+        return True
+    except Exception as e:
+        return False
+
 
 # ============ TEACHERS =============
 def getTeachers():
     cur.execute("SELECT * FROM teachers")
     return cur.fetchall()
 
-# ============ SUBJECTS =============
-def getSubjects():
-    cur.execute("SELECT * FROM subjects")
-    return cur.fetchall()
+def addTeacher(tid, tfname, tlname):
+    try:
+        cur.execute(f"INSERT INTO teachers VALUES ('{tid}', '{tfname}', '{tlname}')")
+        cur.execute(f"INSERT INTO loginusers VALUES ('{tid}','admin',1)")
+        return True
+    except Exception as e:
+        return 
+
+def editTeacher(tid, tfname, tlname):
+    try:
+        cur.execute(f"UPDATE teachers SET teacherFname='{tfname}', teacherLname='{tlname}' WHERE teacherID='{tid}'")
+        return True
+    except Exception as e:
+        return False
 
 # ============ MISC. =============
 def authenticateLogin(username, password):
     cur.execute(f"SELECT * FROM loginusers WHERE userID = '{username}' AND userPassword = '{password}'")
     return cur.fetchall()
 
+
 def getUserDetails(id):
     cur.execute(f"SELECT * FROM students WHERE studentID = '{id}'")
     return cur.fetchall()
+
+# ============ CLASSES =============
+
+def getClassesToEnroll(id):
+    cur.execute(f"SELECT stubCode, subjectID, schedule, CONCAT(timeStart, ' - ', timeEnd) AS Time, teacherLname, (SELECT COUNT(*) FROM classroll WHERE classroll.stubCode = classes.stubCode AND studentID = '{id}') FROM classes LEFT JOIN teachers ON classes.teacherID = teachers.teacherID")
+    return cur.fetchall()
+
+
+def getStudentClasses(userID):
+    cur.execute(f"SELECT classroll.stubCode, classes.subjectID, CONCAT(classes.timeStart, ' - ' , classes.timeEnd) AS Time, classes.schedule, teachers.teacherLname FROM classroll LEFT JOIN classes ON classroll.stubCode = classes.stubCode LEFT JOIN teachers ON classes.teacherID = teachers.teacherID WHERE classroll.studentID = '{userID}'")
+    return cur.fetchall()
+
+
+def enroll(stubCode, userID):
+    cur.execute(f"INSERT INTO classroll(stubCode, studentID) VALUES('{stubCode}', '{userID}')")
+
+
+def getClasses():
+    cur.execute("SELECT * FROM classes")
+    return cur.fetchall()
+
+def addClasses(code, cTid, cSid, sched, start, end):
+    try:
+        cur.execute(f"INSERT INTO classes VALUES({code}, '{cTid}', '{cSid}', '{sched}', '{start}', '{end}')")
+        return True
+    except Exception as e:
+        return False
+
+def editClasses(code, cTid, cSid, sched, start, end):
+    try:
+        cur.execute(f"UPDATE classes SET teacherID='{cTid}',subjectID='{cSid}',schedule='{sched}',timeStart='{start}',timeEnd='{end}' WHERE stubCode='{code}'")
+        return True
+    except Exception as e:
+        return False
+
+# ============ SUBJECTS =============
+# def getSubjects():
+#     cur.execute("SELECT * FROM subjects")
+#     return cur.fetchall()
+
+#     def addTeacher(subjID, subjName):
+#     try:
+#         cur.execute(f"INSERT INTO subjects VALUES ('{subjID}', '{subjName}')")
+#         return True
+#     except Exception as e:
+#         return False
+
+# ============ RATING =============
+
+def rateTeacher(teacherID, userID, rating, comment):
+    cur.execute(f"INSERT INTO ratings(teacherID, studentID, rate, comment) VALUES('{teacherID}', '{userID}', {rating}, '{comment}')")
